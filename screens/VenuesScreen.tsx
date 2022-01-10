8372f13375d81fa38bdb270dc4635dea7bff332b
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2022-01-10 18:25:20 -0500

    Venues List Functional and Aesthetically Okay

--- screens/VenuesScreen.tsx
+++ screens/VenuesScreen.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { FlatList, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { FlatList, Image, ImageBackground, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -35,26 +35,28 @@ export default function VenuesScreen() {
 	
 	return (
 		<>
-			<View style={[styles.header, tw`bg-red-500`]}>
-				<Text style={tw`text-xl text-gray-100 font-bold`}>Venues</Text>
-			</View>
-			<ScrollView style={[styles.container, tw`p-2`]}>
-				<View style={styles.row}>
-					<FlatList
-						data={data}
-						renderItem={({ item }) => (
-							<TouchableOpacity style={[styles.venueContainer, tw`bg-gray-500`]}>
-								{/* <Text style={[styles.venueTitle, tw`text-white`]}> {item.name}</Text> */}
-								<Image
-								style={styles.image}
-								source={item.imageUrl}
-								resizeMode="contain"
-								/>
-							</TouchableOpacity>
-						)}
-					/>
+		<View style={[styles.header, tw`bg-red-500`]}>
+			<Text style={tw`text-xl text-gray-100 font-bold`}>Venues</Text>
+		</View>
+		
+		<FlatList
+		data={data}
+		renderItem={({ item }) => (
+			<ScrollView>
+			<TouchableOpacity style={[styles.venueContainer, tw`bg-gray-500`]}>
+				<ImageBackground
+				style={styles.venueImage}
+				source={item.imageUrl}
+				resizeMode="cover"
+				>
+				<View style={[styles.venueImageOverlay]}>
+					<Text style={[styles.venueTitle]}> {item.name}</Text>
 				</View>
+				</ImageBackground>
+			</TouchableOpacity>
 			</ScrollView>
+		)}
+		/>
 		</>
 	);
 }
@@ -67,33 +69,31 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	container: {
-		flex: 1,
-		backgroundColor: '#1a1a1a',
-	},
-	row: {
-		backgroundColor: 'transparent',
-		flexDirection: 'row',
-		flexWrap: 'wrap',
-		alignItems: 'flex-start', // if you want to fill rows left to right
-		justifyContent: 'center', // if you want to fill rows top to bottom
-	},
 	venueContainer: {
 		width: '90%',
 		height: 150,
 		marginTop: 20,
 		borderRadius: 10,
-		position: 'relative',
+		overflow: 'hidden',
+		alignSelf: 'center',
+	},
+	venueImage : {
+		flex: 1,
+		width: '100%',
+	},
+	venueImageOverlay : {
+		flex: 1,
+		width: '100%',
+		backgroundColor: 'rgba(0,0,0,0.15)',
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-		overflow: 'hidden',
 	},
 	venueTitle : {
-		fontSize: 20,
+		fontSize: 25,
 		fontWeight: 'bold',
+		color: '#fff',
+		paddingHorizontal: 10,
+		textAlign: 'center',
 	},
-	image : {
-		width: '100%',
-	}
 });
