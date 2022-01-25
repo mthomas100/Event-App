ad3913731f06d8de367d80f0c8763340d57b629c
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2022-01-25 14:38:28 -0500

    All events are queried for a city in via useSeatGeekQuery

--- hooks/useSeatGeekQuery.tsx
+++ hooks/useSeatGeekQuery.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Meta, MetaParams } from "../types/meta";
@@ -13,137 +13,56 @@ export type useSeatGeekQueryTypes = (
     data: unknown;
 };
 
-const PER_PAGE_AMOUNT = 100;
-
-
 const useSeatGeekQuery : useSeatGeekQueryTypes = (resource, params) => {
     const [data, setData] = useState<unknown[]>([]);
     const [error, setError] = useState<AxiosError | null>(null);
 	const [loading, setLoading] = useState<boolean>(true);
-
-    // query pagination
-    const [totalPages, setTotalPages] = useState<number | undefined>();
-
-    // useEffect triggers on every change to params
-    const [ effectTrigger, setEffectTrigger ] = useState<boolean>(true);
 
     const endPointUrl = 'https://api.seatgeek.com/2';
     const authParams = {
         client_id: `${process.env.REACT_APP_SEATGEEK_CLIENT_ID}`,
         client_secret: `${process.env.REACT_APP_SEATGEEK_CLIENT_SECRET}`,
     }
-    
-    const metaParams = {
-        per_page: PER_PAGE_AMOUNT,
-    } as Meta;
 
-    const queryData = async (params : Object, metaParams : MetaParams) => {
-        const { data } = await axios.get(`${endPointUrl}/${resource}`, {
+    const fetchData = async (metaParams = {} as MetaParams) => {
+        const res = await axios.get(`${endPointUrl}/${resource}`, {
             params : {
                 ...authParams,
                 ...metaParams,
                 ...params
             }
         });
-        return data;
+        return res;
     }
 
-    useEffect(() => {
-        console.log('useSeatGeekQuery useEffect PARAMS dep');
-        setTotalPages(undefined);
-		const fetchMetaData = async () => {
+    useEffect( () => {
+        let totalPages : number; 
+        const fetchAllData = async () => {
+            // Get totalpages by first by obtaining meta data value of total results and dividing by results per page (and rounding up)
             try {
-            const res = await queryData(params, metaParams as MetaParams) as { meta: Meta };
-            console.log('res', res);
-            setTotalPages(Math.ceil(res.meta.total / res.meta.per_page));
-            // console.log('about to trigger effect', effectTrigger);
-            setEffectTrigger(prev => !prev);
-            } catch {
-                console.log('fetchMetaData error');
+            const res = await fetchData()
+            .then (res => {
+                const totalResults = res.data.meta.total;
+                totalPages = Math.ceil(totalResults / res.data.meta.per_page);
+            });
+            // Get data for all pages and combine into one array
+            const allData = await Promise.all(
+                Array.from({ length: totalPages }, (_, i) => fetchData({ page: i + 1 }))
+            );
+            // Get the resource data from each page and combine into one array
+            const allDataFlattened = allData.reduce((acc, item) => [...acc, ...item.data[resource]], [] as unknown[]);
+            // Set the data state
+            setData(allDataFlattened);
+            } catch (err) {
+                const error = err as AxiosError;
+                setError(error);
+            } finally {
+                setLoading(false);
             }
-        };
-        fetchMetaData();
-    },[params]);
+        }
+        fetchAllData();
+    },[params]); // When city changes (which is a param), trigger effect
 
-    useEffect(() => {
-        // console.log('effect triggered', effectTrigger);
-        console.log('useSeatGeekQuery useEffect PARAMS TOTALPAGES dep 2', totalPages);
-    },[params, totalPages]);
-
-    useEffect(() => {
-        // console.log('useEffect fetchData', {totalPages});
-        // if (totalPages) {
-        //     const fetchData = async () => {
-        //         try {
-        //             (async function fetchAllData() {
-        //                 if (currentPage <= totalPages) {
-        //                     console.log(`fetching page ${currentPage} of ${totalPages}`);
-        //                     const result = await queryData(metaParams as MetaParams);
-        //                     const resourceResult = result[resource] as unknown[];
-        //                     setData(data => [...data, ...resourceResult]);
-        //                     setCurrentPage(prev => prev + 1);
-        //                 }
-        //             }());
-        //             console.log('data', data);
-        //         } catch (error) {
-        //             const err = error as AxiosError;
-        //             setError(err);
-        //             console.error(err);
-        //         }
-        //         // setData(data);
-        //         // setLoading(false);
-        //     };
-        //     fetchData();
-        // }
-    },[params]);
-
-
-    // useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		try { 
-    //             const { meta } = await queryData(metaParams as MetaParams) as { meta: Meta };
-    //             setTotalPages(Math.ceil(meta.total / meta.per_page));
-                
-    //             // (async function fetchAllData () {
-    //             //     if (currentPage <= totalPages) {
-    //             //         console.log(`Fetching page ${currentPage} of ${totalPages}`);
-    //             //         const result = await queryData(metaParams as MetaParams) as { data: unknown[] };
-    //             //         const resultResource = result[resource] as unknown[];
-    //             //         setData(prev => [...prev, ...resultResource]);
-    //             //         setCurrentPage(prev => prev + 1);
-    //             //         fetchAllData();
-    //             //     }
-    //             // }());
-
-    //             // await queryData();
-    //             // setCurrentPage(currentPage + 1);
-    //             // await queryData();
-
-    //             // console.log('metaData', data.meta);
-    //             // console.log('per_page', PER_PAGE_AMOUNT);
-    //             // console.log('pages', Math.ceil(data.meta.total / PER_PAGE_AMOUNT));
-                
-
-    //             // setData(data[resource]);
-    //             // setMetaData(data.meta);
-    //         } catch (error) {
-    //             // TODO: check if error is AxiosError, then handle
-    //             const err = error as AxiosError;
-    //             setError(err);
-    //             console.log(err);
-    //         } finally {
-    //             setLoading(false);
-                
-    //         }
-    //     };
-    //     fetchData();
-    //     console.log('data all fetched', data);
-    // },[params]);
-
-    // useEffect(() => {
-    //     console.log(totalPages);
-    // }, [totalPages]);
-    
     return {
         loading,
         error,
